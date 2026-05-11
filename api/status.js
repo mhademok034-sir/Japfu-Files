5d77d9f0026f2ec8c6c8975af7d963fe9b511c54
mhademok034-sir <mhademok034@gmail.com>
2026-05-11 23:25:46 +0530

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // CURRENT TIME IS: 2026-05-11 23:10
     // 12 HOURS FROM NOW IS: 2026-05-12 11:10 AM
-    const RELEASE_DATE = new Date("2026-05-12T11:10:00").getTime(); 
+    const RELEASE_DATE = new Date("2026-05-11T23:27:00").getTime(); 
     const now = Date.now();
     const timeLeft = Math.max(0, RELEASE_DATE - now);
 
